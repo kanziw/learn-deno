e6cf9e1e5ca1547e63ec9f8f454daba19b7bb691
Jiwoong Jung <kanziwoong@gmail.com>
2020-05-09 14:48:19 +0900

    Test camelCase in params (#3)
    * Test camelCase in params
    
    * Bump denotraion version up

--- 01_denotrain/routes/users.ts
+++ 01_denotrain/routes/users.ts
@@ -16,11 +16,9 @@ router.get('/', async () => {
   return users
 })
 
-// v0.2.1 기준 `/\/:([a-z]+)/g` RegExp 만 가능합니다.
-// https://github.com/Caesar2011/denotrain/blob/28626e1540e22640d60e273330a4dc0571428409/Router.ts#L195
-router.get('/:userid', (ctx) => {
-  const { userid } = ctx.req.param
-  const user = users.find(({ id }) => id === userid)
+router.get('/:userId', (ctx) => {
+  const { userId } = ctx.req.params
+  const user = users.find(({ id }) => id === userId)
   if (!user) {
     throw new ClientError(404, 'Not found user')
   }
